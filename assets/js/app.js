7d29def7138ac116ae90cb9d8ba18a6d7e99493e
Robert Lazić <robert.lazicpz@gmail.com>
2023-02-07 21:39:45 +0100

    modified:   assets/js/app.js 	modified:   assets/style/style.css 	modified:   index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,39 +31,28 @@ const createDeck = () => {
 
 //  VARIABLES  //
 const deck = createDeck();
+const dealerCardsInHand = [];
+let dealerScore = 0;
+const playersCardsInHand = [];
+let playerScore = 0;
 
-////////////////////////////////////////////////////////////////
-// TEST - adding some cards to style table
-const dealer = document.querySelector('.dealer_container');
-const player = document.querySelector('.player_container');
+//  SELECTORS  //
+const dealerHand = document.querySelector('.dealer_hand');
+const displayDealerScore = document.querySelector('#dealer_score');
+const playerHand = document.querySelector('.player_hand');
+const displayPlayerScore = document.querySelector('#player_score');
+const buttonStay = document.querySelector('#stay');
+const buttonCall = document.querySelector('#call');
 
-let card1 = document.createElement('img');
-card1.classList.add('card');
-card1.src = deck[48].image;
-dealer.appendChild(card1);
+// return card as object
+const drawCard = (deck) => {
+  const randomNumber = Math.floor(Math.random() * deck.length);
+  const card = deck.splice(randomNumber, 1);
 
-let card2 = document.createElement('img');
-card2.classList.add('card');
-card2.src = deck[49].image;
-dealer.appendChild(card2);
+  return card[0];
+};
 
-let card3 = document.createElement('img');
-card3.classList.add('card');
-card3.src = deck[48].image;
-player.appendChild(card3);
-
-let card4 = document.createElement('img');
-card4.classList.add('card');
-card4.src = deck[49].image;
-player.appendChild(card4);
-
-let card5 = document.createElement('img');
-card5.classList.add('card');
-card5.src = deck[50].image;
-player.appendChild(card5);
-
-let card6 = document.createElement('img');
-card6.classList.add('card');
-card6.src = deck[51].image;
-player.appendChild(card6);
-////////////////////////////////////////////
+const initialDealingCards = () => {
+  // podijeliti svakom igraču po dvije karte s time da je dilereva druga karta okrenuta prema dolje
+  //igrač izabire da li ce ostati na istim kartama ili ce callat jos jednu
+};
